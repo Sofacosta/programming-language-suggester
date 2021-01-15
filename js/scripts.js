8440999816da67154003bdae97cbe3983f0efec7
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-15 17:13:20 -0600

    add image on top pf page, clean indentation and code and place result inside of form border

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,51 @@
 $(document).ready(function () {
   $("#formOne").submit(function (event) {
     event.preventDefault();
+
     const activity = $("input:radio[name=activity]:checked").val();
+    const interest = $("input:radio[name=interest]:checked").val();
+    const features = $("input:radio[name=features]:checked").val();
+    const character = $("input:radio[name=character]:checked").val();
+    const drawback = $("input:radio[name=drawback]:checked").val();
+
     if (activity === "videogames") {
+      $("#csharp").show();
+    } else if (activity === "musicandvisuals") {
+      $("#python").show();
+    } else {
+      $("#rubyonrails").show();
+    }
+
+    if (interest === "AI") {
+      $("#csharp").show();
+    } else if (interest === "web-development") {
+      $("#python").show();
+    } else {
+      $("#rubyonrails").show();
+    }
+
+    if (features === "easy-learn") {
+      $("#csharp").show();
+    } else if (features === "general-purpose") {
+      $("#python").show();
+    } else {
+      $("#rubyonrails").show();
+    }
+
+    if (character === "Lisa-Simpson") {
+      $("#csharp").show();
+    } else if (character === "Mafalda") {
+      $("#python").show();
+    } else {
+      $("#rubyonrails").show();
+    }
+
+    if (drawback === "few-platform") {
+      $("#csharp").show();
+    } else if (drawback === "debugging-task") {
       $("#python").show();
     } else {
       $("#rubyonrails").show();
     }
   });
 });
-
-//const activity = $("input:radio[name=activity]:checked").val();
-//const activity = $("input:radio[name=activity]:checked").val();
-//const activity = $("input:radio[name=activity]:checked").val();
-//const activity = $("input:radio[name=activity]:checked").val();
