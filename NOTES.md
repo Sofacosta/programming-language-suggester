fdd3d972aa412678b84cf747f85e0d2ce52044ac
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-15 14:17:32 -0600

    Fix bug in question 1: create a general div class and make js code work. When you submit an option you recieve an answer of a programming language you should learn

---
+++ NOTES.md
@@ -0,0 +1,4 @@
+# Notes
+## Error with 
+* Error Selector: .find() selector: https://api.jquery.com/find/#find-selector
+* Input checked: https://api.jquery.com/checked-selector/
--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
 $(document).ready(function () {
-  $("form#formOne").submit(function() {
-  const activity = $("input:radio[name=activity]:checked").val();
-  //const activity = $("input:radio[name=activity]:checked").val();  
-  //const activity = $("input:radio[name=activity]:checked").val(); 
-  //const activity = $("input:radio[name=activity]:checked").val(); 
-  //const activity = $("input:radio[name=activity]:checked").val(); 
-event.preventDefault();
-
-if (activity === videogames) {
- $(#python).show();
-} else {
-  $("#rubyonrails").show();
-  }
-
-
-    
+  $("#formOne").submit(function () {
+    event.preventDefault();
+    const activity = $(".activity").find("input[type=radio]:checked").val();
+    if (activity === "videogames") {
+      $("#python").show();
+    } else {
+      $("#rubyonrails").show();
+    }
+  });
 });
 
-
+//const activity = $("input:radio[name=activity]:checked").val();
+//const activity = $("input:radio[name=activity]:checked").val();
+//const activity = $("input:radio[name=activity]:checked").val();
+//const activity = $("input:radio[name=activity]:checked").val();
